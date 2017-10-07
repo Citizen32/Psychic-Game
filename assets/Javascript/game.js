7132320d481e743ce38f7597e81632f17d752e00
David Morales <morgar.creative@gmail.com>
2017-10-07 06:37:04 -0500

    Created my variables in Javascript

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -1,1 +1,12 @@
-alert("Hello world")
+//This is an array of the alphabet and they will be our guess options.
+var compChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+//These are the variables for our input(user) and output(computer).
+var wins = 0;
+var losses = 0;
+var guesses = 10;
+var guessesLeft = 10;
+var guessedLetter= [];
+
+var random = compChoices [Math.floor(Math.random() * compChoices.lenght);
+]
